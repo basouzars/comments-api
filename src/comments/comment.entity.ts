5e9e1aa99593c1c3ae3a7ecaa8c787ab01a1f757
Bruno Aguiar <bruno.souza@accessloans.com>
2024-07-20 14:30:53 -0300

    dasdsapojk

--- src/comments/comment.entity.ts
+++ src/comments/comment.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   Entity,
   Column,
@@ -10,38 +11,53 @@ import {
 
 @Entity()
 export class Comment {
+  @ApiProperty({ type: 'string', format: 'uuid' })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty()
   @Column()
   module: string;
 
+  @ApiProperty()
   @Column()
   requestId: string;
 
+  @ApiProperty()
   @Column()
   userId: string;
 
+  @ApiProperty()
   @Column()
   userName: string;
 
+  @ApiProperty()
   @Column('text')
   comment: string;
 
+  @ApiProperty()
   @CreateDateColumn()
   createdAt: Date;
 
+  @ApiProperty()
   @UpdateDateColumn()
   updatedAt: Date;
 
+  @ApiProperty({ required: false })
   @Column({ nullable: true })
   updatedByUserId: string;
 
+  @ApiProperty({ required: false })
   @Column({ nullable: true })
   deletedAt: Date;
 
+  @ApiProperty({ required: false })
   @Column({ nullable: true })
   deletedByUserId: string;
+
+  @ApiProperty({ required: false })
+  @Column({ nullable: true })
+  parentCommentId: string;
 
   @ManyToOne(() => Comment, (comment) => comment.id)
   parentComment: Comment;
